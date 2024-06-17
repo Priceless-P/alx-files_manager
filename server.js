fe417f8954b93e5687c2ba7dad59aedb34dac9d7
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-06-17 15:32:35 +0100

    Fix

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+// eslint-disable-next-line jest/require-hook
 app.use(express.json());
 
+// eslint-disable-next-line jest/require-hook
 routing(app);
 
 // eslint-disable-next-line jest/require-hook

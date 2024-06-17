a3112f568010ba277f4b471f2b64f37c755cfeb8
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2024-06-17 14:40:29 +0100

    Task 2

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,38 @@
-const mongodb = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor () {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const userCollection = this.client.db().collection('users');
+      return await userCollection.countDocuments();
+    } catch (err) {
+      console.error('Error counting users:', err);
+      return 0;
     }
+  }
+
+  async nbFiles() {
+    try {
+      const fileCollection = this.client.db().collection('files');
+      return await fileCollection.countDocuments();
+    } catch (err) {
+      console.error('Error counting files:', err);
+      return 0;
+    }
+  }
 }
 
 module.exports = new DBClient();
